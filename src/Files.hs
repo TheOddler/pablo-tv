7549001c9174755b0bf96e795db434d7cf79bc2a
Pablo Bollansée <pablo.bollansee@gmail.com>
2024-08-16 02:36:52 +0100

    Attempt at parsing file names for information

--- src/Files.hs
+++ src/Files.hs
@@ -1,13 +1,22 @@
 module Files where
 
-import Data.Text (Text)
-import Text.Regex (matchRegex, mkRegex)
+import Control.Applicative ((<|>))
+import Data.Text (Text, breakOn, isPrefixOf, strip, unpack)
+import Data.Text qualified as T
+import GHC.Utils.Misc (fstOf3)
+import Text.Read (readMaybe)
+import Text.Regex.TDFA
 
 data EpisodeInfo = EpisodeInfo
-  { episodeTitle :: Maybe Text,
-    episodeNumber :: Int,
-    episodeSeason :: Int
+  { episodeSeason :: Int,
+    episodeNumber :: EpisodeNumber,
+    episodeSpecial :: Bool
   }
+  deriving (Eq, Show)
+
+data EpisodeNumber
+  = EpisodeNumber Int
+  | EpisodeNumberDouble Int Int
   deriving (Eq, Show)
 
 data MovieInfo = MovieInfo
@@ -21,11 +30,105 @@ data FileInfo
   | FileMovie MovieInfo
   deriving (Eq, Show)
 
-parseFileName :: Int -> [Text] -> FileInfo
-parseFileName _indexInDir segments =
-  let (_folderName, fileName) =
-        case reverse segments of
-          [] -> ("", "")
-          [x] -> ("", x)
-          (fileN : folderN : _) -> (folderN, fileN)
-   in FileMovie $ MovieInfo fileName Nothing
+parseFileName :: Int -> Int -> [Text] -> FileInfo
+parseFileName indexInDir fileCount segments =
+  let (folder, file) = case reverse segments of
+        [] -> ("", "")
+        [x] -> ("", x)
+        (fileN : folderN : _) -> (folderN, fileN)
+
+      readInt :: Text -> Maybe Int
+      readInt = readMaybe . unpack . strip
+
+      seasonFromFolder :: Maybe Int
+      seasonFromFolder
+        | "Season" `isPrefixOf` folder = readInt $ T.drop 6 folder
+        | "Series" `isPrefixOf` folder = readInt $ T.drop 6 folder
+        | otherwise = Nothing
+
+      tryRegexFolder :: ([Text] -> Maybe a) -> Text -> Maybe a
+      tryRegexFolder f regex =
+        let res :: (Text, Text, Text, [Text])
+            res = folder =~ regex
+            (_, _, _, matches) = res
+         in f matches
+
+      tryRegex :: ([Text] -> Maybe a) -> Text -> Maybe a
+      tryRegex f regex =
+        let res :: (Text, Text, Text, [Text])
+            res = file =~ regex
+            (_, _, _, matches) = res
+         in f matches
+
+      expect1Int :: [Text] -> Maybe Int
+      expect1Int = \case
+        [a] ->
+          readInt a
+        _ -> Nothing
+
+      expect2Ints :: [Text] -> Maybe (Int, Int)
+      expect2Ints = \case
+        [a, b] ->
+          (,) <$> readInt a <*> readInt b
+        _ -> Nothing
+
+      expect3Ints :: [Text] -> Maybe (Int, Int, Int)
+      expect3Ints = \case
+        [a, b, c] ->
+          (,,) <$> readInt a <*> readInt b <*> readInt c
+        _ -> Nothing
+
+      seasonFromFile :: Maybe Int
+      seasonFromFile =
+        tryRegex expect1Int "[Ss]eason ([0-9]+)"
+          <|> tryRegex expect1Int "[Ss]eries ([0-9]+)"
+
+      doubleEpisodeFromFile :: Maybe (Int, Int, Int)
+      doubleEpisodeFromFile =
+        tryRegex expect3Ints "[Ss]([0-9]+)[Ee]([0-9]+)-[Ee]([0-9]+)"
+
+      episodeInfoFromFile :: Maybe (Int, Int)
+      episodeInfoFromFile =
+        tryRegex expect2Ints "[Ss]([0-9]+)[Ee]([0-9]+)"
+          <|> tryRegex expect2Ints "([0-9]+)[Xx]([0-9]+)"
+
+      episodeFromFile :: Maybe Int
+      episodeFromFile =
+        tryRegex expect1Int "[Ee]pisode ([0-9]+)"
+          <|> tryRegex expect1Int "[Aa]flevering ([0-9]+)"
+          <|> tryRegex expect1Int "[Ss]pecial ([0-9]+)"
+
+      season =
+        seasonFromFolder
+          <|> fstOf3 <$> doubleEpisodeFromFile
+          <|> fst <$> episodeInfoFromFile
+          <|> seasonFromFile
+
+      isSpecial :: Bool
+      isSpecial =
+        (fstOf3 <$> doubleEpisodeFromFile <|> fst <$> episodeInfoFromFile) == Just 0
+          || file =~ ("[Ss]pecial" :: Text)
+
+      (confidentEpisode, episode) = case (doubleEpisodeFromFile, episodeInfoFromFile, episodeFromFile) of
+        (Just (_, a, b), _, _) -> (True, EpisodeNumberDouble a b)
+        (Nothing, Just (_, a), _) -> (True, EpisodeNumber a)
+        (Nothing, Nothing, Just a) -> (True, EpisodeNumber a)
+        (Nothing, Nothing, Nothing) -> (False, EpisodeNumber (indexInDir + 1))
+
+      movieYear =
+        let yearRegex = "((19|20)[0-9][0-9])"
+         in fst
+              <$> ( tryRegexFolder expect2Ints yearRegex
+                      <|> tryRegex expect2Ints yearRegex
+                  )
+
+      movieTitle = case folder of
+        "" -> strip . fst $ breakOn "(" file
+        _ -> strip . fst $ breakOn "(" folder
+   in case (season, confidentEpisode) of
+        (Just s, _) -> FileEpisode $ EpisodeInfo s episode isSpecial
+        (Nothing, True) -> FileEpisode $ EpisodeInfo 1 episode isSpecial
+        -- This next rule is a bit silly, might remove it
+        -- Maybe it makes more sense to not try to parse movies and series from the same folder?
+        (Nothing, _) | fileCount >= 2 -> FileEpisode $ EpisodeInfo 1 episode True
+        (Nothing, False) -> FileMovie $ MovieInfo movieTitle movieYear
--- test/FilesSpec.hs
+++ test/FilesSpec.hs
@@ -8,25 +8,207 @@ spec :: Spec
 spec = do
   describe "Correctly parses episodes" $
     mapM_ fileNameSpec $
-      mapThird FileEpisode <$> episodeExamples
+      mapFourth FileEpisode <$> episodeExamples
 
   describe "Correctly parses movies" $
     mapM_ fileNameSpec $
-      mapThird FileMovie <$> movieExamples
+      mapFourth FileMovie <$> movieExamples
 
-mapThird :: (c -> d) -> (a, b, c) -> (a, b, d)
-mapThird f (a, b, c) = (a, b, f c)
+mapFourth :: (d -> e) -> (a, b, c, d) -> (a, b, c, e)
+mapFourth f (a, b, c, d) = (a, b, c, f d)
 
-episodeExamples :: [(Int, String, EpisodeInfo)]
-episodeExamples = []
+episodeExamples :: [(Int, Int, String, EpisodeInfo)]
+episodeExamples =
+  [ ( 3,
+      6,
+      "Pabloland/Season 2/Pabloland.s02e04.hdtv.x264-tla.mp4",
+      EpisodeInfo 2 (EpisodeNumber 4) False
+    ),
+    ( 3,
+      6,
+      "Pabloland/Season 2/Pabloland.s02e02.XviD-AFG.avi",
+      EpisodeInfo 2 (EpisodeNumber 2) False
+    ),
+    ( 999,
+      20,
+      "Pabloland/Season 2/Pabloland.S02E06.hdtv.x264-tla.mp4",
+      EpisodeInfo 2 (EpisodeNumber 6) False
+    ),
+    ( 7,
+      7,
+      "Pabloland/Season 1/Pabloland [01x04] The Episode Name.mp4",
+      EpisodeInfo 1 (EpisodeNumber 4) False
+    ),
+    ( 8,
+      10,
+      "Pabloland/Season 3/Pabloland.S03E09.Pabloland.Christmas.720p.HDTV.x264-GOGOGO[series].mkv",
+      EpisodeInfo 3 (EpisodeNumber 9) False
+    ),
+    ( 999,
+      10,
+      "My TV Series/Episode 6 - Episode Name (My TV Series) [abcd].mp4",
+      EpisodeInfo 1 (EpisodeNumber 6) False
+    ),
+    ( 2,
+      4,
+      "My TV Series/NEW My TV Series Episode Name [xyza].mp4",
+      EpisodeInfo 1 (EpisodeNumber 3) True
+    ),
+    ( 0,
+      8,
+      "Happy/Season 2/happy.s02e04.720p.hdtv.x264-tla.mp4",
+      EpisodeInfo 2 (EpisodeNumber 4) False
+    ),
+    ( 0,
+      6,
+      "The Pablos/The Pablo s1e5.mp4",
+      EpisodeInfo 1 (EpisodeNumber 5) False
+    ),
+    ( 1,
+      8,
+      "SeriesName/Season 4/SeriesName, s4e5, EpName.mp4",
+      EpisodeInfo 4 (EpisodeNumber 5) False
+    ),
+    ( 1,
+      3,
+      "SeriesName/Season 4/SeriesName, Special s0e13, Christmas Special, Special Name.mp4",
+      EpisodeInfo 4 (EpisodeNumber 13) True
+    ),
+    ( 1,
+      5,
+      "SeriesName/Season 4/SeriesName, s0e13, Its The Name.mp4",
+      EpisodeInfo 4 (EpisodeNumber 13) True
+    ),
+    ( 1,
+      5,
+      "Good Series/Season 1/Good.Series.S01E01.720p.WEBRiP.x264-ABCD[pppp].mkv",
+      EpisodeInfo 1 (EpisodeNumber 1) False
+    ),
+    ( 1,
+      5,
+      "Streaming Stuff-1 (1994)/Season 4/Streaming Stuff-1 - S04E05 - Binge It All (1080p x265 YEAR2020).mkv",
+      EpisodeInfo 4 (EpisodeNumber 5) False
+    ),
+    ( 1,
+      5,
+      "Streaming Stuff-1 (1994)/Season 1/Streaming Stuff-1 - S01E01-E02 - Watching It All (1080p x265 YEAR2020).mkv",
+      EpisodeInfo 1 (EpisodeNumberDouble 1 2) False
+    ),
+    ( 1,
+      5,
+      "Streaming Stuff-1 (1994)/Season 1/Streaming Stuff-1 - S00E01-E02 - Another Day Lost (1080p x265 YEAR2020).mkv",
+      EpisodeInfo 1 (EpisodeNumberDouble 1 2) True
+    ),
+    ( 1,
+      5,
+      "Bum Bam (Good Animation)/Alternate Takes/Bum Bam Season 1 - From here to There.mp4",
+      EpisodeInfo 1 (EpisodeNumber 2) False
+    ),
+    ( 1,
+      5,
+      "Bum Bam (Good Animation)/Season 2/Bum Bam S02E25 Small House....mp4",
+      EpisodeInfo 2 (EpisodeNumber 25) False
+    ),
+    ( 1,
+      5,
+      "Bum Bam (Good Animation)/VA Commentary/Bum Bam Episode S02E02 - Commentary.mp4",
+      EpisodeInfo 2 (EpisodeNumber 2) False
+    ),
+    ( 1,
+      5,
+      "Nukes/Nukes s1e4.mp4",
+      EpisodeInfo 1 (EpisodeNumber 4) False
+    ),
+    ( 1,
+      5,
+      "Nukes/Nukes s1e6.mp4",
+      EpisodeInfo 1 (EpisodeNumber 6) False
+    ),
+    ( 1,
+      5,
+      "Vlaamschen Serie/Season 1/Vlaamschen Serie - Aflevering 2.avi",
+      EpisodeInfo 1 (EpisodeNumber 2) False
+    ),
+    ( 1,
+      5,
+      "Vlaamschen Serie/Season 1/Vlaamschen Serie - Aflevering 4.avi",
+      EpisodeInfo 1 (EpisodeNumber 4) False
+    ),
+    ( 1,
+      5,
+      "British Series/Season 2/British Series - S02E03 - EpNaMe - [WEBDL-1080p h265].mkv",
+      EpisodeInfo 2 (EpisodeNumber 3) False
+    ),
+    ( 1,
+      5,
+      "Sad Politics/Series 4/Sad Politics, Series 4, Episode 5 [bombam].mp4",
+      EpisodeInfo 4 (EpisodeNumber 5) False
+    ),
+    ( 1,
+      5,
+      "Sad Politics/Series 2/Sad Politics, Series 2 Special 1: Ugh ffs [123456].mp4",
+      EpisodeInfo 2 (EpisodeNumber 1) True
+    ),
+    ( 1,
+      5,
+      "I Like Dinos/I.Like.Dinos.2022.S01E01.1080p.x265-QWER.mkv",
+      EpisodeInfo 1 (EpisodeNumber 1) False
+    ),
+    ( 1,
+      5,
+      "Name Of Series/Season 2/name.of.series.s02e06.720p.hdtv.x264-tla.mkv",
+      EpisodeInfo 2 (EpisodeNumber 6) False
+    )
+  ]
 
-movieExamples :: [(Int, String, MovieInfo)]
-movieExamples = []
+movieExamples :: [(Int, Int, String, MovieInfo)]
+movieExamples =
+  [ ( 1,
+      1,
+      "Film Van Mijn Jeugd/Film Van Mijn Jeugd.mkv",
+      MovieInfo "Film Van Mijn Jeugd" Nothing
+    ),
+    ( 1,
+      1,
+      "Film Van Mijn Jeugd/Film Van Mijn Jeugd Part 2.webm",
+      MovieInfo "Film Van Mijn Jeugd" Nothing
+    ),
+    ( 1,
+      1,
+      "Film Van Mijn Jeugd/Film Van Mijn Jeugd Part 1.webm",
+      MovieInfo "Film Van Mijn Jeugd" Nothing
+    ),
+    ( 1,
+      1,
+      "Film Van Mijn Jeugd/Film Van Mijn Jeugd.webm",
+      MovieInfo "Film Van Mijn Jeugd" Nothing
+    ),
+    ( 1,
+      1,
+      "Tweede In De Reeks/Tweede.In.De.Reeks.2044.1080p.WEBRip.1600MB.DD5.1.x264-Goooooo.mkv",
+      MovieInfo "Tweede In De Reeks" (Just 2044)
+    ),
+    ( 1,
+      1,
+      "Eerste In De Reeks/Eerste.In.De.Reeks.2016.1080p.HDR.Bluray.AV1.Multi4-smartperson.mkv",
+      MovieInfo "Eerste In De Reeks" (Just 2016)
+    ),
+    ( 1,
+      1,
+      "Japanese Movie (2023, Dubbed)/Japanese.Movie.2023.DUBBED.1080p.AMZN.WEBRip.DD5.1.x264-Woooops.mkv",
+      MovieInfo "Japanese Movie" (Just 2023)
+    ),
+    ( 1,
+      1,
+      "Parent Parent/Parent (1991)/Actual File Here (1991).avi",
+      MovieInfo "Parent" (Just 1991)
+    )
+  ]
 
-fileNameSpec :: (Int, String, FileInfo) -> Spec
-fileNameSpec (indexInDir, fileName, expected) =
+fileNameSpec :: (Int, Int, String, FileInfo) -> Spec
+fileNameSpec (indexInDir, fileCount, fileName, expected) =
   it ("Correctly parses " <> fileName) $ do
     let segments :: [Text]
         segments = splitOn "/" $ pack fileName
 
-    parseFileName indexInDir segments `shouldBe` expected
+    parseFileName indexInDir fileCount segments `shouldBe` expected
