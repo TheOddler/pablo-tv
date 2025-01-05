7e0561dff4acdf3389bd08b01fc51c00c82cb031
Pablo Bollansée <pablo.bollansee@gmail.com>
2025-01-05 21:29:14 +0000

    Move arrow nav code to a julius file so yesod updates it properly on change

--- src/LibMain.hs
+++ src/LibMain.hs
@@ -60,7 +60,6 @@ mkEmbeddedStatic
   False
   "embeddedStatic"
   [ embedFile "static/reconnecting-websocket.js",
-    embedFile "static/arrow-navigation.js",
     embedFile "static/fontawesome/css/all.min.css",
     embedFile "static/fontawesome/webfonts/fa-brands-400.ttf",
     embedFile "static/fontawesome/webfonts/fa-brands-400.woff2",
@@ -103,7 +102,6 @@ instance Yesod App where
     pc <- widgetToPageContent $ do
       when isDevelopment $ addScriptRemote "https://pabloproductions.be/LiveJS/live.js"
       addScript $ StaticR static_reconnecting_websocket_js
-      addScript $ StaticR static_arrow_navigation_js
       addStylesheet $ StaticR static_fontawesome_css_all_min_css
       $(widgetFile "shared-default")
     withUrlRenderer $
