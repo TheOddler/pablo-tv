2acbf5138815084d8b54df700d3aea7e79f4a0ec
Pablo Bollansée <pablo.bollansee@gmail.com>
2026-02-01 16:08:43 +0000

    Add basic yesod test

---
+++ test/WebSpec.hs
@@ -0,0 +1,34 @@
+module WebSpec where
+
+import Actions (mkInputDevice)
+import Foundation
+import GHC.Conc (newTVarIO)
+import LibMain ()
+import PVar (newPVar)
+import TVState (startingTVState)
+import Test.Syd
+import Test.Syd.Yesod
+
+mkTestApp :: IO App
+mkTestApp = do
+  inputDevice <- mkInputDevice
+  tvState <- newTVarIO startingTVState
+  lastActivePlayerTVar <- newTVarIO Nothing
+  rootDirsPVar <- newPVar mempty
+  pure
+    App
+      { appPort = 8080,
+        appLogFunc = \_ _ -> pure (),
+        appInputDevice = inputDevice,
+        appTVState = tvState,
+        appGetStatic = embeddedStatic,
+        appLastActivePlayer = lastActivePlayerTVar,
+        appRootDirs = rootDirsPVar
+      }
+
+spec :: Spec
+spec = do
+  yesodSpecWithSiteGenerator mkTestApp $ do
+    it "returns 200 on the homepage" $ do
+      get HomeR
+      statusIs 200
