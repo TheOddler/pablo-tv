0888f0872fa12d58868826f4502fab1a4af0b0bf
Pablo Bollansée <pablo.bollansee@gmail.com>
2026-06-06 19:57:17 +0100

    Add hot reloading

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 # This script run both the backend and frontend, and auto-reload both on change
+# The backend serves the frontend at its root, but it doesn't support hot-reloading.
+# To have hot-reloading you need to open the page elm-live hosts, it will output the url for that on start.
 
 set -euo pipefail
 
 backend_cmd="watchexec -e hs,hamlet,cabal --restart cabal run backend -f development"
-frontend_cmd="(cd frontend && elm-live src/Main.elm --dir=static --proxy-prefix=/api --proxy-host=http://localhost:8080 -- --output=static/main.js)"
+frontend_cmd="(cd frontend && elm-live src/Main.elm --hot --dir=static --proxy-prefix=/api --proxy-host=http://localhost:8080 -- --output=static/main.js)"
 
 parallel --line-buffer \
   --tagstring "{1}" --xapply {2} ::: "🌐" "🌳" \
