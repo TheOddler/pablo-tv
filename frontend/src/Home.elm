4e666171e708daead430d63066fbe6de37be886a
Pablo Bollansée <pablo.bollansee@gmail.com>
2026-06-11 19:50:43 +0100

    Home viewHome to home file

--- frontend/src/Home.elm
+++ frontend/src/Home.elm
@@ -4,6 +4,7 @@ import AggDir exposing (AggDirInfo)
 import Generated.Backend exposing (..)
 import Html exposing (..)
 import Html.Attributes as A
+import Random
 
 
 viewRow : List AggDirInfo -> Html msg
@@ -36,4 +37,36 @@ viewPoster dir =
                     ++ "/"
                     ++ String.fromInt dir.videoFileCount
             ]
+        ]
+
+
+view : RootDirectories -> Int -> Html msg
+view roots startTime =
+    let
+        aggInfos =
+            AggDir.calcForSubDirsOfRoots roots
+
+        seed1 =
+            Random.initialSeed startTime
+
+        seed2 =
+            Random.initialSeed <| startTime + 42
+
+        row filter sorting =
+            viewRow <|
+                AggDir.filterAndSort filter sorting aggInfos
+    in
+    div [ A.id "home-container" ]
+        [ h1 [] [ text "Watching" ]
+        , row AggDir.Watching AggDir.RecentlyWatched
+        , h1 [] [ text "New" ]
+        , row AggDir.NothingWatched AggDir.RecentlyAdded
+        , h1 [] [ text "Random" ]
+        , row AggDir.NothingWatched (AggDir.Shuffled seed1)
+        , h1 [] [ text "Recently Added" ]
+        , row AggDir.Unfiltered AggDir.RecentlyAdded
+        , h1 [] [ text "Random (All)" ]
+        , row AggDir.Unfiltered (AggDir.Shuffled seed2)
+        , h1 [] [ text "Recently Finished" ]
+        , row AggDir.FullyWatched AggDir.RecentlyWatched
         ]
--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -1,14 +1,11 @@
 module Main exposing (Msg(..), main, update, view)
 
-import AggDir
 import Browser
 import Dict
 import Generated.Backend exposing (..)
 import Home
 import Html exposing (..)
-import Html.Attributes as A
 import Http
-import Random
 
 
 type alias Model =
@@ -71,38 +68,6 @@ view model =
     { title = "Pablo TV"
     , body =
         [ text <| String.join ", " <| Dict.keys model.data
-        , viewHome model
+        , Home.view model.data model.startTime
         ]
     }
-
-
-viewHome : Model -> Html Msg
-viewHome model =
-    let
-        aggInfos =
-            AggDir.calcForSubDirsOfRoots model.data
-
-        seed1 =
-            Random.initialSeed model.startTime
-
-        seed2 =
-            Random.initialSeed <| model.startTime + 42
-
-        viewRow filter sorting =
-            Home.viewRow <|
-                AggDir.filterAndSort filter sorting aggInfos
-    in
-    div [ A.id "home-container" ]
-        [ h1 [] [ text "Watching" ]
-        , viewRow AggDir.Watching AggDir.RecentlyWatched
-        , h1 [] [ text "New" ]
-        , viewRow AggDir.NothingWatched AggDir.RecentlyAdded
-        , h1 [] [ text "Random" ]
-        , viewRow AggDir.NothingWatched (AggDir.Shuffled seed1)
-        , h1 [] [ text "Recently Added" ]
-        , viewRow AggDir.Unfiltered AggDir.RecentlyAdded
-        , h1 [] [ text "Random (All)" ]
-        , viewRow AggDir.Unfiltered (AggDir.Shuffled seed2)
-        , h1 [] [ text "Recently Finished" ]
-        , viewRow AggDir.FullyWatched AggDir.RecentlyWatched
-        ]
