63efee4344c6f2fb4101aa89295b25dff64167b5
Pablo Bollansée <pablo.bollansee@gmail.com>
2026-06-16 22:20:39 +0100

    Make the css hot-reloading more robust by making it just consider main.css and always removing all elements in case the js is reloaded in the middle of hot-reloading the css as that could result in duplicating the css and eventually building up a huge list of them

--- frontend/postprocess-hot-css.js
+++ frontend/postprocess-hot-css.js
@@ -2,44 +2,50 @@
 // It's polling the server, and supports `If-Modified-Since` as my server return 304 when there are no changes.
 const hotReloadingCode = `
 (function () {
-  const last = new Map(); // url -> last-modified value
-  async function check(link) {
+  // Note that since all my css builds to main.css, I only consider that file for hot-reloading, which makes the script much simpler
+  const url = "/main.css";
+  let last = null;
+
+  async function check() {
     try {
-      const url = link.href.split('?')[0];
       const res = await fetch(
         url,
         {
           method: 'HEAD',
-          headers: last.has(url) ? { 'If-Modified-Since': last.get(url) } : {},
+          headers: last ? { 'If-Modified-Since': last } : {},
           cache: 'no-store'
         }
       );
       if (res.status === 304) return; // unchanged
       if (!res.ok) return; // error
       // Remember the last-modified date the server sends
-      last.set(url, res.headers.get('Last-Modified'));
-      reloadCss(link, url);
+      last = res.headers.get('Last-Modified');
+      reloadCss();
     } catch (err) {
       console.error("Error while reloading CSS", err);
     }
   };
 
-  function reloadCss(link, url) {
-    // Creating a new element like this allows the browser to pre-load the css, preventing flicker on changes
-    const replacement = link.cloneNode();
-    replacement.href = url + '?_hot=' + Date.now();
-    replacement.onload = () => {
-      link.remove();
+  function reloadCss() {
+    // Creating a new element like this allows the browser to pre-load the css, preventing flicker on changes.
+    // It's also possible that if the JS reloaded in the middle of this, we ended up with a new element without having removed the old one. So always remove ALL old ones.
+    let allExisting = Array.from(document.querySelectorAll('link[rel="stylesheet"][href]')).filter(l => l.href.split('?')[0].endsWith(url));
+
+    const newOne = document.createElement('link');
+    newOne.rel = 'stylesheet';
+    newOne.href = url + '?_hot=' + Date.now();
+
+    newOne.onload = () => {
+      allExisting.forEach(l => l.remove());
     };
-    replacement.onerror = (e) => {
-      replacement.remove();
+    newOne.onerror = (e) => {
+      newOne.remove();
     };
-    link.parentNode.insertBefore(replacement, link.nextSibling);
+    document.head.appendChild(newOne);
   }
 
   async function tick() {
-    const allCss = Array.from(document.querySelectorAll('link[rel="stylesheet"][href]'));
-    await Promise.all(allCss.map(check));
+    await check();
     setTimeout(tick, 1000);
   }
   tick();
